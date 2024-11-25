aed765119abca2e0f9c992bde55778b54d66d1d9
Justen <me@justenmanni.dev>
2024-11-26 00:02:38 +0800

    Add seperation of functions - helper_functions.h and helper_functions.cpp

---
+++ include/helper_function.h
@@ -0,0 +1,21 @@
+#ifndef HELPER_FUNCTION_H
+#define HELPER_FUNCTION_H
+
+#include "hardware_init.h"
+
+// ENUM STATUS
+enum Status
+{
+    SAFE,
+    WARNING,
+    DANGER
+};
+
+void testBuzzer();
+void testNeoPixels();
+void checkSafetyAndAlert(float lpg, float co, float smoke);
+void setNeoPixelStatus(Status status);
+float convertRawSoundToDecibels(int rawValue);
+void checkWiFi();
+
+#endif
